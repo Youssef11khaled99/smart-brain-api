7b16247a73217ab7e0cb58db5bf87512bf071241
youssef11khaled99 <uossefkhaled99@gmail.com>
2019-08-18 21:07:21 +0200

    correct spelling mistake

--- server.js
+++ server.js
@@ -39,6 +39,6 @@ app.put('/image', (req, res) => { image.handleImagePut(req, res, db) })
 
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res, db) })
 
-app.listen(processes.env.PORT || 3000, () => {
-    console.log(`app is running in port ${processes.env.PORT}`);
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`app is running in port ${process.env.PORT}`);
 })
